e8bd0615d70bb6950797a2beaf741ea1500e6183
bol-bbang <hh.park@kakao.com>
2021-07-24 22:53:08 +0900

    add method and test code for detail in RestaurantController

--- eatgo-api/src/main/java/com/ojt/green/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/ojt/green/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package com.ojt.green.eatgo.interfaces;
 
 import com.ojt.green.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,5 +17,17 @@ public class RestaurantController {
         restaurants.add(new Restaurant(1004L,"Green", "an-yang"));
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id){
+        List<Restaurant> restaurants = new ArrayList<Restaurant>();
+        restaurants.add(new Restaurant(1004L,"Green", "an-yang"));
+        restaurants.add(new Restaurant(1005L,"Shin-jun", "an-yang"));
+
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
--- eatgo-api/src/test/java/com/ojt/green/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/ojt/green/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,5 +1,6 @@
 package com.ojt.green.eatgo.interfaces;
 
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -24,8 +25,28 @@ class RestaurantControllerTest {
                 containsString("\"name\":\"Green\"")
         ))
         .andExpect(content().string(
-                containsString("")
+                containsString("\"id\":1004")
         ));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        StringContains.containsString("\"id\":1004")
+                ))
+                .andExpect(content().string(
+                        StringContains.containsString("\"name\":\"Green\"")
+                ));
+        mvc.perform(get("/restaurants/1005"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        StringContains.containsString("\"id\":1005")
+                ))
+                .andExpect(content().string(
+                        StringContains.containsString("\"name\":\"Shin-jun\"")
+                ));
     }
 
 }
